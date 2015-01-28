f68d97936fc72a218fd5b0c6c37f49ce46d1b94d
Phillip Wong <pwong@paybyphone.com>
2015-01-28 15:03:16 -0800

    + Added in a hacked together WebApi assembly just to see what changes would need to be made.

---
+++ log4net.Raygun.WebApi/RaygunAppender.cs
@@ -0,0 +1,12 @@
+﻿using log4net.Raygun.Core;
+using Mindscape.Raygun4Net.WebApi;
+
+namespace log4net.Raygun.WebApi
+{
+    public class RaygunAppender : RaygunAppenderBase
+    {
+        public RaygunAppender() : base(new RaygunWebApiMessageBuilder(), apiKey => new RaygunWebApiClientAdapter(new RaygunWebApiClient(apiKey)))
+        {
+        }
+    }
+}
