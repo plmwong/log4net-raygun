cd928ba16b8b3f211c516a9a6df882b24a18eb0b
Mitchell <mitchell@raygun.com>
2019-10-11 14:51:10 +1300

    Fix issues relating to the Raygun4Net upgrade
    Fix issues relating to the Raygun4Net upgrade

--- log4net.Raygun.Core/IgnoredDataSettings.cs
+++ log4net.Raygun.Core/IgnoredDataSettings.cs
@@ -4,15 +4,22 @@ using System.Linq;
 namespace log4net.Raygun.Core
 {
 	public sealed class IgnoredDataSettings
-	{
-		public IgnoredDataSettings(string ignoredFormNames = null, string ignoredHeaderNames = null,
-            string ignoredCookieNames = null, string ignoredServerVariableNames = null, bool isRawDataIgnored = false)
+	{		
+		public IgnoredDataSettings(string ignoredFormNames = null,
+			                         string ignoredHeaderNames = null,
+			                         string ignoredCookieNames = null,
+			                         string ignoredServerVariableNames = null,
+			                         string ignoredQueryParameterNames = null,
+			                         string ignoredSensitiveFieldNames = null,
+			                         bool isRawDataIgnored = false)
 		{
-			IgnoredFormNames = SplitIgnoredSetting(ignoredFormNames);
-			IgnoredHeaderNames = SplitIgnoredSetting(ignoredHeaderNames);
-			IgnoredCookieNames = SplitIgnoredSetting(ignoredCookieNames);
+			IgnoredFormNames           = SplitIgnoredSetting(ignoredFormNames);
+			IgnoredHeaderNames         = SplitIgnoredSetting(ignoredHeaderNames);
+			IgnoredCookieNames         = SplitIgnoredSetting(ignoredCookieNames);
 			IgnoredServerVariableNames = SplitIgnoredSetting(ignoredServerVariableNames);
-            IsRawDataIgnored = isRawDataIgnored;
+			IgnoredQueryParameterNames = SplitIgnoredSetting(ignoredQueryParameterNames);
+			IgnoredSensitiveFieldNames = SplitIgnoredSetting(ignoredSensitiveFieldNames);
+      IsRawDataIgnored           = isRawDataIgnored;
 		}
 
 		private IEnumerable<string> SplitIgnoredSetting(string setting) 
@@ -20,15 +27,19 @@ namespace log4net.Raygun.Core
 			return string.IsNullOrEmpty(setting) ? Enumerable.Empty<string>() : setting.Split(',').ToList();
 		}
 
-		public IEnumerable<string> IgnoredFormNames { get; private set; }
+		public IEnumerable<string> IgnoredFormNames { get; set; }
 
-		public IEnumerable<string> IgnoredHeaderNames { get; private set; }
+		public IEnumerable<string> IgnoredHeaderNames { get; set; }
 
-		public IEnumerable<string> IgnoredCookieNames { get; private set; }
+		public IEnumerable<string> IgnoredCookieNames { get; set; }
 
-		public IEnumerable<string> IgnoredServerVariableNames { get; private set; }
-
-        public bool IsRawDataIgnored { get; private set; }
+		public IEnumerable<string> IgnoredServerVariableNames { get; set; }
+		
+		public IEnumerable<string> IgnoredQueryParameterNames { get; set; }
+		
+		public IEnumerable<string> IgnoredSensitiveFieldNames { get; set; }
+		
+		public bool IsRawDataIgnored { get; set; }
 	}
 }
 
